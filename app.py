ad11551997a228a771f643ad422f3731b35b64c7
demarillacizere <izered3@gmail.com>
2023-03-11 14:01:00 +0200

    changed the model

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, request,  render_template
 import joblib
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 app = Flask(__name__)
 
 # load the trained model
-model = joblib.load("fifa_model.joblib")
+model = joblib.load("model.joblib")
 
 @app.route('/')
 def index():
@@ -28,13 +29,40 @@ def predict():
         return render_template('index.html', message='Only CSV files are allowed')
     
     file = request.files['file']
-    columns = ['attacking_crossing', 'attacking_finishing', 'attacking_heading_accuracy', 'attacking_short_passing', 'attacking_volleys', 'skill_dribbling', 'skill_curve', 'skill_fk_accuracy', 'skill_long_passing', 'skill_ball_control', 'movement_acceleration', 'movement_sprint_speed', 'movement_agility', 'movement_reactions', 'movement_balance', 'power_shot_power', 'power_jumping', 'power_stamina', 'power_strength', 'power_long_shots', 'mentality_aggression', 'mentality_interceptions', 'mentality_positioning', 'mentality_vision', 'mentality_penalties', 'mentality_composure', 'defending_marking_awareness', 'defending_standing_tackle', 'defending_sliding_tackle', 'goalkeeping_diving', 'goalkeeping_handling', 'goalkeeping_kicking', 'goalkeeping_positioning', 'goalkeeping_reflexes']
-    data = pd.read_csv(file)
-    y_pred = model.predict(data[columns])
-    y_pred = np.round(y_pred)
-    realvalues = data['overall'].iloc[data.index]
+    columns =['age', 'attacking_crossing', 'attacking_short_passing', 'dribbling', 'international_reputation', 'mentality_aggression', 'mentality_composure', 'mentality_vision', 'movement_reactions', 'passing', 'physic', 'potential', 'power_long_shots', 'power_shot_power', 'release_clause_eur', 'shooting', 'skill_ball_control', 'skill_curve', 'skill_long_passing', 'value_eur', 'wage_eur']
+    df = pd.read_csv(file)
 
-    return render_template('index.html', predictions=list(y_pred)[:10],realvalues=list(realvalues)[:10])
+    df.drop(['sofifa_id', 'player_url', 'club_name', 'nationality_name', 'long_name', 'real_face', 'nation_team_id', 'player_tags', 'player_traits', 'player_face_url', 'club_logo_url', 'club_flag_url', 'nation_logo_url', 'nation_flag_url', 'club_joined'], axis=1, inplace=True)
+
+    # Converting string columns to numerical using label encoding
+    le = LabelEncoder()
+    df['preferred_foot'] = le.fit_transform(df['preferred_foot'])
+    df['work_rate'] = le.fit_transform(df['work_rate'])
+    df['body_type'] = le.fit_transform(df['body_type'])
+    df['club_position'] = le.fit_transform(df['club_position'].fillna('NaN'))
+    df['player_positions'] = le.fit_transform(df['player_positions'])
+    df['league_name'] = le.fit_transform(df['league_name'])
+    df['dob'] = le.fit_transform(df['dob'])
+    # df['short_name'] = le.fit_transform(df['short_name'])
+
+    # Filling empty values with mean or mode
+    df['release_clause_eur'].fillna(df['release_clause_eur'].mean(), inplace=True)
+    df['height_cm'].fillna(df['height_cm'].mean(), inplace=True)
+    df['weight_kg'].fillna(df['weight_kg'].mean(), inplace=True)
+    df['pace'].fillna(df['pace'].mode()[0], inplace=True)
+    df['shooting'].fillna(df['shooting'].mode()[0], inplace=True)
+    df['passing'].fillna(df['passing'].mode()[0], inplace=True)
+    df['dribbling'].fillna(df['dribbling'].mode()[0], inplace=True)
+    df['defending'].fillna(df['defending'].mode()[0], inplace=True)
+    df['physic'].fillna(df['physic'].mode()[0], inplace=True)
+    
+    df.fillna(df.mean(numeric_only=True).round(1), inplace=True)
+
+    y_pred = model.predict(df[columns])
+    y_pred = np.round(y_pred)
+    realvalues = df['overall'].iloc[df.index]
+    names = df['short_name'].iloc[df.index]
+    return render_template('index.html', predictions=list(y_pred)[:10],realvalues=list(realvalues)[:10],names=list(names)[:10])
 
 if __name__ == '__main__':
     app.run(debug=True)
